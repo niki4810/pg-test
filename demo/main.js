a1c40daf13dd4894f4b5a8665464c76700a3d5bc
Nikhilesh Katakam <nkatakam@modeln.com>
2013-10-11 13:46:51 -0700

    demo3

---
+++ demo/main.js
@@ -0,0 +1,97 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nkatakam
+ * Date: 10/1/13
+ * Time: 11:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+"use strict";
+require.config({
+	baseUrl:"http://niki4810.github.io/pg-test/demo",
+    paths : {
+        "jquery" : "lib/jquery",
+        "jqueryui" : "lib/jquery-ui",
+        "underscore" : "lib/underscore",
+        "backbone" : "lib/backbone",
+        "marionette":'lib/backbone-marionette',
+        "backbone.wreqr":'lib/backbone-wreqr',
+        "backbone.babysitter":'lib/backbone-babysitter',
+        "geppetto" : "lib/backbone-geppetto",
+        "text" : "lib/text",
+        "handlebars" : "lib/handlebars",
+        "datatables" : "lib/datatables/jquery.dataTables"
+    },
+   // urlArgs: "bust=" + (new Date()).getTime(),
+    shim : {
+        "jquery" : {
+            exports : "$"
+        },
+        "jqueryui" : {
+            deps : ["jquery"]
+        },
+        "underscore" : {
+            exports : "_"
+        },
+        "backbone" : {
+            deps : ["underscore", "jquery"],
+            exports : "Backbone"
+        },     
+        "lib/backbone-modelbinder" :{
+            deps : ["backbone"]
+        },
+        "lib/jquery.mockjson" : {
+        	deps : ["jquery"]
+        },
+        "lib/jquery.hammer" : {
+        	deps : ["jquery"]
+   
+		}
+
+		},
+	config: {
+    	'text': 
+    		{ 
+    			useXhr: function (url, protocol, hostname, port) {
+              	//return true if you want to allow this url, given that the
+              	//text plugin thinks the request is coming from protocol,  hostname, port.
+              	return true;
+            	}
+    		}
+  		}
+});
+
+define([
+    "jquery",
+    "jqueryui",
+    "underscore",
+    "backbone",
+    "marionette",
+    "geppetto",
+    "src/app/CMnApp",
+    "lib/backbone-modelbinder",
+    "handlebars",
+    "lib/jquery.hammer",    
+    "datatables",
+    "lib/jquery.mockjson"
+], function(
+    $,
+    jqueryUI,
+    _,
+    Backbone,
+    Marionette,
+    Geppetto,
+    CMnApp) {
+    $(function() {
+        Backbone.Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
+            return Handlebars.compile(rawTemplate);
+        };
+
+        $.noConflict(true);
+   		_.noConflict();
+    	Backbone.noConflict();
+
+    	return CMnApp.start();
+
+
+    });
+});
